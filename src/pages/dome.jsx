e368828708c4475695d326597aaef30c0fb8952c
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-03-23 15:28:45 +0800

    fix

--- src/pages/dome.jsx
+++ src/pages/dome.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import { Button } from 'antd';
-// import { NewInput } from 'component-library-hyx/src/components';
-import { addSome, Debounce } from 'npm-react-dom';
-import { Errors } from '@/components';
+import { NewInput } from 'component-library-hyx';
 function Example() {
   const [count, setCount] = useState(0);
-  console.log(addSome(1, 2), '1111');
   return (
     <div>
       <p>You clicked {count} times</p>
-      {/* <NewInput /> */}
+      <NewInput></NewInput>
       <Button type="primary" onClick={() => setCount(count + 1)}>
         Click me
       </Button>
