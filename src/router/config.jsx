1f1d00715b099b621d71e0f529f8901dc8b57dc0
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-06-24 10:35:33 +0800

    fix

--- src/router/config.jsx
+++ src/router/config.jsx
@@ -1,27 +1,38 @@
+import PropTypes from 'prop-types';
+
 const { Provider, Consumer } = React.createContext({});
 
 class Component extends React.Component {
-  static defaultProps = {
-    children: [],
-    defaultPreloadLoading: null,
-    globalData: {},
-  };
+	static defaultProps = {
+		children: [],
+		defaultPreloadLoading: null,
+		globalData: {},
+	};
 
-  static Consumer = Consumer;
+	static propTypes = {
+		children: PropTypes.oneOfType([
+			PropTypes.node,
+			PropTypes.arrayOf(PropTypes.node),
+		]),
+		defaultPreloadLoading: PropTypes.element,
+		globalData: PropTypes.object,
+	};
 
-  render() {
-    const { children, defaultPreloadLoading, globalData } = this.props;
-    return (
-      <Provider
-        value={{
-          defaultPreloadLoading,
-          globalData,
-        }}>
-        {children}
-      </Provider>
-    );
-  }
+	static Consumer = Consumer;
+
+	render() {
+		const { children, defaultPreloadLoading, globalData } = this.props;
+		return (
+			<Provider
+				value={{
+					defaultPreloadLoading,
+					globalData,
+				}}
+			>
+				{children}
+			</Provider>
+		);
+	}
 }
-
 
 export default Component;
--- src/router/lazy.jsx
+++ src/router/lazy.jsx
@@ -1,25 +1,29 @@
+import PropTypes from 'prop-types';
 import Config from './config';
 
 const { lazy, Suspense } = React;
 
 class Component extends React.Component {
+  static propTypes = {
+    load: PropTypes.object.isRequired,
+  };
 
-	render() {
-		const { load } = this.props;
-		const LazyComponent = lazy(() => load);
-		return (
-			<Config.Consumer>
-				{({ defaultPreloadLoading }) => {
-					const renderComponent = (
-						<Suspense fallback={defaultPreloadLoading || <span>加载中...</span>}>
-							<LazyComponent {...this.props}/>
-						</Suspense>
-					);
-					return renderComponent;
-				}}
-			</Config.Consumer>
-		);
-	}
+  render() {
+    const { load } = this.props;
+    const LazyComponent = lazy(() => load);
+    return (
+      <Config.Consumer>
+        {({ defaultPreloadLoading }) => {
+          const renderComponent = (
+            <Suspense fallback={defaultPreloadLoading || <span>加载中...</span>}>
+              <LazyComponent {...this.props} />
+            </Suspense>
+          );
+          return renderComponent;
+        }}
+      </Config.Consumer>
+    );
+  }
 }
 
 export default Component;
